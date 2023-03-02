b485314614584e1051ae2508f418cf2c5b72dc38
Paolo Giovannini <giovannini13@gmail.com>
2023-03-02 15:55:36 +0100

    esercizio pari e dispari concluso

--- pari e dispari/js/main.js
+++ pari e dispari/js/main.js
@@ -1,3 +1,4 @@
+let sceltaPariODispari = prompt("Scegli pari o dispari")
 
 const sceltaNumero = parseInt(prompt("Scegli un numero da 1 a 5"));
 console.log(`il numero scelto è ${sceltaNumero}`);
@@ -8,9 +9,26 @@ console.log(`il numero scelto dal computer è ${numeroCasualeComputer}`);
 const sommaNumeri = sceltaNumero + numeroCasualeComputer;
 console.log(`la somma è ${sommaNumeri}`);
 
+const controlloNumeroPariODispari = pariDispari(sommaNumeri)
+
+if (sceltaPariODispari === controlloNumeroPariODispari) {
+    console.log('Ha vinto il giocatore');
+} else {
+    console.log('Ha vinto il computer');
+}
 
 function generaNumeroCasuale(min, max) {
     const numeroCasuale = Math.floor( Math.random() * (max - min - 1) ) + min;
     return numeroCasuale;
 }
 
+function pariDispari(SommaNumeroUtenteComputer) {
+
+    if (SommaNumeroUtenteComputer % 2 == 0) {
+       return "pari";
+    } else {
+        return "dispari"
+    }
+
+
+}
